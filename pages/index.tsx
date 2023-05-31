25cd2384983734e7f6fbb8daeee7e7e16be40f28
doruk <doruksahin98@gmail.com>
2023-06-01 01:25:55 +0300

    init

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,62 +1,172 @@
-import Brand from '@/components/Brand';
-import Hamburger from '@/components/Hamurger';
-import Menu from '@/components/Menu';
-import NodeScreen from '@/components/NodeScreen';
+import React, {useEffect, useState} from 'react';
+import usePrevious from '@/hooks/usePrevious';
 
-import {useEffect, useState} from 'react';
+type Square = {
+  top: number;
+  bottom: number;
+  left: number;
+  right: number;
+  index: number;
+};
 
-// h-screen, why it does not affect children?
-// px vs rem
-// overflow hidden olmayınca mt-4 üstteki classı etkiliyor ne alaka???
-// nbsp olayını not al - https://stackoverflow.com/questions/1762539/margin-on-child-element-moves-parent-element
+type Point = {
+  x: number;
+  y: number;
+};
 
-export default function Home() {
-  const [content, setContent] = useState<string | null>(null);
-  const [showMenu, setShowMenu] = useState<boolean>(false);
-  const [bgColor, setBgColor] = useState<boolean>(true);
-  const [isMobileScreen, setIsMobileScreen] = useState<boolean>(false);
-  useEffect(() => {
-    if (window.innerWidth > 768) {
-      setShowMenu(true);
-    } else {
-      setIsMobileScreen(true);
-    }
-  }, []);
+export default function Grid() {
+  const LENGTH = 10;
 
-  const onMenuItemWithContentClick = () => {
-    if (isMobileScreen) {
-      setShowMenu(!showMenu);
-      setBgColor(!bgColor);
-    }
+  const [isDragging, setIsDragging] = useState<boolean | null>(null);
+  const previousIsDragging = usePrevious(isDragging);
+  const [candidateIndexes, setCandidateSquares] = useState<number[]>([]);
+  const [squareIndexesList, setSquareIndexesList] = useState([]);
+  const isDraggingStopped = previousIsDragging == true && isDragging == false;
+  const squareProperties = processSquareIndexes(squareIndexesList);
+  const squareIndexesToBeSkipped = squareIndexesList
+    .map(squareIndexes => squareIndexes.slice(0))
+    .flat()
+    .sort((a, b) => a - b);
+  const mergedSquareIndexes = squareIndexesList.flat();
+  const completedSquarePreview = completeSquareShape(candidateIndexes);
+
+  const handleMouseDown = (index: number) => {
+    setIsDragging(true);
+    setCandidateSquares([index]);
   };
 
+  const handleMouseEnter = (index: number) => {
+    if (!isDragging) return;
+    setCandidateSquares([...candidateIndexes, index]);
+  };
+
+  useEffect(() => {
+    if (!isDraggingStopped) return;
+    const completedSquareShape = completeSquareShape(candidateIndexes);
+    setCandidateSquares([]);
+    if (
+      completedSquareShape.some(candidateSquare =>
+        mergedSquareIndexes.includes(candidateSquare),
+      )
+    )
+      return;
+    setSquareIndexesList(prev => [...prev, completedSquareShape]);
+  }, [isDraggingStopped, candidateIndexes, mergedSquareIndexes]);
+
+  function completeSquareShape(candidateSquareIndexes: number[]) {
+    const copyCandidateSquareIndexes = [...candidateSquareIndexes];
+    if (copyCandidateSquareIndexes.length < 2) {
+      return copyCandidateSquareIndexes;
+    }
+    const floorNumbers = copyCandidateSquareIndexes
+      .map(copyCandidateSquareItem =>
+        Math.floor(copyCandidateSquareItem / LENGTH),
+      )
+      .sort((a, b) => a - b);
+    const firstFloor = floorNumbers[0]!;
+    const lastFloor = floorNumbers[floorNumbers.length - 1]!;
+    for (const candidateSquareItem of copyCandidateSquareIndexes) {
+      const candidateSquareItemFloor = Math.floor(candidateSquareItem / LENGTH);
+      for (let i = candidateSquareItemFloor; i < lastFloor; i++) {
+        const newNumber = candidateSquareItem + LENGTH;
+        if (!copyCandidateSquareIndexes.includes(newNumber)) {
+          copyCandidateSquareIndexes.push(newNumber);
+        }
+      }
+      for (let i = candidateSquareItemFloor; i > firstFloor; i--) {
+        const newNumber = candidateSquareItem - LENGTH;
+        if (!copyCandidateSquareIndexes.includes(newNumber)) {
+          copyCandidateSquareIndexes.push(newNumber);
+        }
+      }
+    }
+    if (
+      copyCandidateSquareIndexes.some(candidateSquare =>
+        mergedSquareIndexes.includes(candidateSquare),
+      )
+    ) {
+      return [];
+    }
+    const sorted = copyCandidateSquareIndexes.sort((a, b) => a - b);
+    return sorted;
+  }
+
+  function processSquareIndexes(squareIndexesList: number[][]) {
+    const processedSquares = [];
+    for (const squareIndexes of squareIndexesList) {
+      let consecutiveNumberCount = 1;
+      for (let i = 0; i < squareIndexes.length - 1; i++) {
+        if (squareIndexes[i + 1] - squareIndexes[i] == 1) {
+          consecutiveNumberCount += 1;
+        } else {
+          break;
+        }
+      }
+
+      const yLength = squareIndexes.length / consecutiveNumberCount;
+      processedSquares.push({
+        xLength: consecutiveNumberCount,
+        yLength: yLength,
+        index: squareIndexes[0],
+      });
+    }
+    return processedSquares;
+  }
   return (
     <div
-      className="max-h-full"
+      className="grid h-screen"
+      onMouseLeave={() => setIsDragging(false)}
+      onMouseDown={() => setIsDragging(true)}
+      onMouseUp={() => {
+        setIsDragging(false);
+      }}
       style={{
-        backgroundColor: `${bgColor ? '#eae8e5' : '#bdb7ad'}`,
-        transition: 'background-color 2s ease',
+        gridTemplateColumns: `repeat(${LENGTH}, minmax(0, 1fr))`,
+        gridAutoRows: 'minmax(0, 1fr)',
       }}>
-      <Menu
-        setContent={setContent}
-        showMenu={showMenu}
-        onMenuItemWithContentClick={onMenuItemWithContentClick}></Menu>
-      <div className="h-screen">
-        <div className="h-1/6">
-          <div className="flex justify-between">
-            <Brand showMenu={showMenu} style="md:hidden block" />
-            <div className=" mr-4 mt-4 md:hidden">
-              <Hamburger
-                onClick={() => {
-                  setShowMenu(!showMenu);
-                  setBgColor(!bgColor);
-                }}
-              />
+      {new Array(LENGTH * LENGTH).fill(null).map((square, index) => {
+        const filteredSquareProperties = squareProperties.filter(
+          squareProperty => squareProperty.index == index,
+        );
+        const squareProperty =
+          filteredSquareProperties.length > 0
+            ? filteredSquareProperties[0]
+            : null;
+        const shouldSkip = squareIndexesToBeSkipped.includes(index);
+        if (squareProperty) {
+          return (
+            <div
+              onMouseDown={() => handleMouseDown(index)}
+              onMouseEnter={() => handleMouseEnter(index)}
+              key={index}
+              className=" flex items-center justify-center border border-black bg-gray-200"
+              style={{
+                gridRow: `span ${squareProperty.yLength}`,
+                gridColumn: `span ${squareProperty.xLength}`,
+              }}>
+              {index}
             </div>
-          </div>
-        </div>
-        {content ? <>{content}</> : <NodeScreen />}
-      </div>
+          );
+        } else if (shouldSkip) {
+          return <></>;
+        } else {
+          return (
+            <div
+              onMouseDown={() => handleMouseDown(index)}
+              onMouseEnter={() => handleMouseEnter(index)}
+              key={index}
+              className={`flex select-none items-center justify-center border  ${
+                completedSquarePreview.includes(index)
+                  ? 'bg-red-200'
+                  : 'bg-gray-100'
+              }`}>
+              {index}
+            </div>
+          );
+        }
+      })}
+
+      {/* Add more grid cells as needed */}
     </div>
   );
 }
