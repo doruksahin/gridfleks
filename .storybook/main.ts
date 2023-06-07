7f637376eed779125c8cba78c09274d92cb08a83
doruk <doruksahin98@gmail.com>
2023-06-07 15:14:03 +0300

    adds storybook - integrated with tailwindcss and ts path

---
+++ .storybook/main.ts
@@ -0,0 +1,38 @@
+import type { StorybookConfig } from "@storybook/nextjs";
+import TsconfigPathsPlugin from 'tsconfig-paths-webpack-plugin';
+const path = require('path');
+
+const baseUrl = './'; // Update this to match your tsconfig.json baseUrl
+
+const config: StorybookConfig = {
+  stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/nextjs",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+  webpackFinal: async (config, { configType }) => {
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader']
+    }),
+    config.resolve.plugins = [
+      ...(config.resolve.plugins || []),
+      new TsconfigPathsPlugin({
+        configFile: path.resolve(__dirname, '../tsconfig.json'),
+        extensions: config.resolve.extensions,
+        baseUrl,
+      }),
+    ];
+
+    return config;
+}
+};
+export default config;
